b41c1ec9023b0f517e5bbd58264e5bb6d5312f71
GproSoftware828 <JetSendProductions@Gmail.com>
2019-01-18 17:11:30 +1100

    Added missing connection to menu classes

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,11 +6,14 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
-
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.open];
+  }
   return (
     <Aux>
       <Backdrop show={props.open} clicked={props.closed} />
-      <div className={classes.SideDrawer}>
+      <div className={attachedClasses.join(' ')}>
         <div className={classes.Logo}>
           <Logo />
         </div>
