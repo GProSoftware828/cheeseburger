dea6739cda286677fed6c734b8d0827f1348db06
GproSoftware828 <JetSendProductions@Gmail.com>
2019-01-19 13:43:11 +1100

    Added order submission

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,34 +93,11 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    // //alert('You are continuing!');
-    // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   //real app- recalculate price on order so person can't change price
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'GP',
-    //     address: {
-    //       street: '123 Main',
-    //       zipCode: '38420',
-    //       country: 'Australia'
-    //     },
-    //     email: 'test@test.com'
-    //   },
-    //   deliveryMethod: 'fastest'
-    // };
-    // axios.post('/orders.json', order)
-    //   .then(response => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   })
-    //   .catch(error => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   });
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
+    queryParams.push('price=' + this.state.totalPrice);
     const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: '/checkout',
