ea1872947637f2c95577f72d152495cbc2e271a4
GproSoftware828 <JetSendProductions@Gmail.com>
2019-01-20 17:40:32 +1100

    Purchase comment- initial state not resetting to false automatically

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -3,6 +3,7 @@ import * as actionTypes from '../actions/actionTypes';
 const initialState = {
   orders: [],
   loading: false,
+  //not automatically resetting to false unless re-cache
   purchased: false
 };
 
